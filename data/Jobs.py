afcb006d020555bdf1bccfa19b941da9458dac79
Dima <grmgorer@gmail.com>
2025-05-06 16:35:10 +0300

    добовление работы

--- data/Jobs.py
+++ data/Jobs.py
@@ -7,10 +7,9 @@ from .db_session import SqlAlchemyBase
 
 class Jobs(SqlAlchemyBase):
     __tablename__ = 'jobs'
-    id = sqlalchemy.Column(sqlalchemy.Integer,
+    id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"),
                            primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.Integer,
-                                    sqlalchemy.ForeignKey("users.id"))
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer)
     job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.Integer,
                                   default=0)
@@ -18,5 +17,6 @@ class Jobs(SqlAlchemyBase):
     start_date = sqlalchemy.Column(sqlalchemy.Integer)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
     user = orm.relationship('User')
+
     def __str__(self):
         return f'{self.id}'
--- data/departments.py
+++ data/departments.py
@@ -7,11 +7,10 @@ from sqlalchemy_serializer import SerializerMixin
 class Department(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'departments'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
+    id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"),
                            primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    chief = sqlalchemy.Column(sqlalchemy.Integer,
-                              sqlalchemy.ForeignKey("users.id"))
+    chief = sqlalchemy.Column(sqlalchemy.Integer)
     members = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     user = orm.relationship('User')
